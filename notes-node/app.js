672b91faf6014e842f458cda65701c5c2b0a4431
Shubham <shub.shubhamthakur@gmail.com>
2018-05-15 03:21:17 +0530

    lodash 3rd party module practised, _.isString() and _.uniq()

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,11 +2,22 @@ console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
-
+const _ = require('lodash');
 const notes = require('./notes.js');
-var user = os.userInfo();
-var res = notes.add(9,-3);
-console.log("Result: "+res);
+
+var filter = _.uniq(['Shub','shub','Shub',1,2,3,1,'shub']);
+console.log(filter);
+
+
+
+//console.log(_.isString(true));
+//console.log(_.isString('Shubham'));
+
+
+
+//var user = os.userInfo();
+//var res = notes.add(9,-3);
+//console.log("Result: "+res);
 //console.log(user);
 /*fs.appendFile('greeting.txt','\nHello ' + user.username+'!'+' You are '+notes.age+'.',function(err){
   if(err){
