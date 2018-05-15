8a9fea273753b13a56f0c63280b1f472f59f48e9
Shubham <shub.shubhamthakur@gmail.com>
2018-05-16 01:53:01 +0530

    practise JSON methods to convert string to Object and vice-versa

---
+++ notes-node/playground/json.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+/*var obj = {
+  name : "Shubham",
+  age : 24
+}
+
+var stringObj = JSON.stringify(obj);
+console.log(typeof stringObj);
+console.log(stringObj);
+
+var jObj = JSON.parse(stringObj);
+console.log(typeof jObj);
+console.log(jObj);
+*/
