d4ccb2b4ee2760526e16f56ab49f70e099b8ecb5
Shubham <shub.shubhamthakur@gmail.com>
2019-02-25 01:06:03 +0530

    taking user inputs for address and fetching lat and lng for the address

--- weather/weather/app.js
+++ weather/weather/app.js
@@ -1,9 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
 
 
+var argv = yargs.
+	options({
+		a:{
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias("help","h")
+	.argv;
 
+console.log(argv);
+
+var uriAddress = encodeURIComponent(argv.a);
+console.log(uriAddress);
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=sant%20nagar%20burari%20delhi%20india&key=AIzaSyATsyM4tYmZO6tDSv1S1C6Qkjras1WZI7k',
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+uriAddress+'&key=AIzaSyATsyM4tYmZO6tDSv1S1C6Qkjras1WZI7k',
 	json: true
 },(error, response, body)=>{
 	//console.log(JSON.stringify(body,undefined,4));
