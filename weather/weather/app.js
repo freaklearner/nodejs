fae910d06613a209eadd2e3850ce06dbec51e020
Shubham <shub.shubhamthakur@gmail.com>
2019-02-25 00:37:26 +0530

    getting address components from result to google geocoding api

--- weather/weather/app.js
+++ weather/weather/app.js
@@ -6,5 +6,8 @@ request({
 	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=sant%20nagar%20burari%20delhi%20india&key=AIzaSyATsyM4tYmZO6tDSv1S1C6Qkjras1WZI7k',
 	json: true
 },(error, response, body)=>{
-	console.log(body);
+	//console.log(JSON.stringify(body,undefined,4));
+	console.log("Address: "+body.results[0].formatted_address);
+	console.log("Lat: "+body.results[0].geometry.location.lat);
+	console.log("Lng: "+body.results[0].geometry.location.lng);
 });
